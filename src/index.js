78fb2d2929b9557f1c7fe7d8b3d5eb5906b67743
Rob Sassack <rsassack25@gmail.com>
2022-08-22 23:07:43 -0400

    Added function to fetch current weather from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, weather!');
+import { apiKey } from './config';
+
+async function getWeather(location) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey.key}`);
+  const data = await response.json();
+  return data;
+}
+
+getWeather('Detroit').then((data) => {
+  console.log(data);
+});
