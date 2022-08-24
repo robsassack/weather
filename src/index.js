1eb4d1c4e316693cbc99aab5263e7a17574e2f11
Rob Sassack <rsassack25@gmail.com>
2022-08-23 23:00:30 -0400

    Added user input for location

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { apiKey } from './config';
 
+const locationInput = document.querySelector('#location');
+const submit = document.querySelector('#submit');
+
 async function getWeather(location) {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey.key}`);
   const data = await response.json();
@@ -8,4 +11,11 @@ async function getWeather(location) {
 
 getWeather('Detroit').then((data) => {
   console.log(data);
+});
+
+submit.addEventListener('click', () => {
+  getWeather(locationInput.value).then((data) => {
+    console.log(data);
+  });
+  locationInput.value = '';
 });
