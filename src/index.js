888f6425ffbe9e21d030e28f737bf03029385834
Rob Sassack <rsassack25@gmail.com>
2022-08-24 23:34:43 -0400

    Added output to document

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { apiKey } from './config';
 
 const locationInput = document.querySelector('#location');
 const submit = document.querySelector('#submit');
+const content = document.querySelector('.content');
 
 async function getWeather(location) {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey.key}`);
@@ -9,13 +10,50 @@ async function getWeather(location) {
   return data;
 }
 
+function kelvinToFahrenheit(kelvin) {
+  return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+}
+
+function meterSecToMilesHour(meterSec) {
+  return (meterSec * 2.237).toFixed(0);
+}
+
+function displayData(data) {
+  content.textContent = '';
+
+  const location = document.createElement('h2');
+  location.textContent = `${data.name}, ${data.sys.country}`;
+  content.appendChild(location);
+
+  const temperature = document.createElement('p');
+  temperature.textContent = `${kelvinToFahrenheit(data.main.temp)}°`;
+  content.appendChild(temperature);
+
+  const weather = document.createElement('p');
+  weather.textContent = `${data.weather[0].main} (${data.weather[0].description})`;
+  content.appendChild(weather);
+
+  const highLow = document.createElement('p');
+  highLow.textContent = `High: ${kelvinToFahrenheit(data.main.temp_max)}° | Low: ${kelvinToFahrenheit(data.main.temp_min)}°`;
+  content.appendChild(highLow);
+
+  const humidity = document.createElement('p');
+  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+  content.appendChild(humidity);
+
+  const wind = document.createElement('p');
+  wind.textContent = `Wind: ${meterSecToMilesHour(data.wind.speed)} mph`;
+  content.appendChild(wind);
+}
+
 getWeather('Detroit').then((data) => {
   console.log(data);
+  displayData(data);
 });
 
 submit.addEventListener('click', () => {
   getWeather(locationInput.value).then((data) => {
-    console.log(data);
+    displayData(data);
   });
   locationInput.value = '';
 });
